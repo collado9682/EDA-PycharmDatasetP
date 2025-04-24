946109778ca8a42a46b991fe88bc2fd2c8a8c41c
collado9682 <genesiscollado813@gmail.com>
2025-04-24 13:48:40 -0600

    Mostrando las primeras cinco filas del archivo

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,7 +1,11 @@
+# 1. Importar las librerías necesarias
 import pandas as pd
+import matplotlib.pyplot as plt
 
-#import matplotlib.pyplot as plt
-
+# 2. Leer el archivo CSV
 df_movies= pd.read_csv("./Data/Rotten Tomatoes Movies.csv")
 
+# 3. Mostrar las primeras filas del DataFrame para revisar su contenido
+print(df_movies.head())
 print(df_movies.dtypes)
+
