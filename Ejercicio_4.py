edcd1f7c95ef4ba205a24f88045417e70d96a8fe
collado9682 <genesiscollado813@gmail.com>
2025-04-24 22:03:54 -0600

    Primeros avances incisos 1 y 2

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -1,7 +1,22 @@
-import pandas as pd
+# 4.	Análisis por Género
+# Muchas películas tienen múltiples géneros. Aquí deberás a separar esos géneros y analizar promedios por cada uno.
+# •	Este paso se requiere manejar géneros múltiples. Primero, crea una copia de tu DataFrame para no modificar el original.
+# •	En la copia, divide la columna genre por “,” para obtener una lista de géneros para cada película y expande estas listas para que cada género tenga su propia fila. Pista: Usa .str.split(',').explode(). Asigna el resultado generado en un nuevo DataFrame “df_genres_exploded”.
+# •	Usando df_genres_exploded, calcula el promedio de audience_rating para cada género individual. Pista: Usa groupby('genre')['audience_rating'].mean().
+# •	Muestra los 10 géneros con el promedio de calificación de audiencia más alto haciendo uso de un diagrama de pastel.
 
-#import matplotlib.pyplot as plt
+#Importar las librerías
+import pandas as pd
+import matplotlib.pyplot as plt
 
 df_movies= pd.read_csv("./Data/Rotten Tomatoes Movies.csv")
 
-print(df_movies.dtypes)
+df_movies_copy=df_movies.copy()
+
+#separar los géneros
+
+df_genres_exploded = df_movies_copy.assign(
+  genre=df_movies_copy['genre'].str.split(',')
+).explode('genre')
+
+#print(df_movies_copy)
