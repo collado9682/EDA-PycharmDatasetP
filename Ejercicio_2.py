175b788bd732e42c6a0d1cc4b175c9171bdf6d95
collado9682 <genesiscollado813@gmail.com>
2025-04-24 20:56:33 -0600

    Avances ejercicio 2, inciso 8

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,7 +1,16 @@
-import pandas as pd
+# 2.	Exploración Básica
+# Se realizará una exploración inicial del dataset para conocer su tamaño, los géneros más comunes y las categorías de calificación.
+# •	¿Cuántas películas hay? Muestra el número total de filas en el DataFrame (que corresponde al número de películas). Pista: Usa len() o df_movies.shape.
+# •	¿Cómo se distribuyen las calificaciones? Cuenta cuántas películas pertenecen a cada categoría en tomatometer_status (Certified Fresh, Fresh, Rotten) usando value_counts().
+# •	Visualiza la distribución de calificaciones: Crea un gráfico circular (pie chart) simple para mostrar la proporción de cada tomatometer_status. Asegúrate de añadir etiquetas.
 
-#import matplotlib.pyplot as plt
+#IMPORTAR LAS LIBRERIAS
+
+import pandas as pd
+import matplotlib.pyplot as plt
 
 df_movies= pd.read_csv("./Data/Rotten Tomatoes Movies.csv")
+#print(df_movies)
 
-print(df_movies.dtypes)
+
+
