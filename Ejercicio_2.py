67d0b88df7fed3a499d759592f9eb44399b88474
collado9682 <genesiscollado813@gmail.com>
2025-04-24 21:08:59 -0600

    Finalizando los incisos 9 y 10

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -16,4 +16,15 @@ df_movies= pd.read_csv("./Data/Rotten Tomatoes Movies.csv")
 
 print(f"Total de peliculas: {len(df_movies)}")
 
+# 9. ¿Cuántas películas fueron calificadas como "Certified Fresh", "Fresh" y "Rotten"? usa value counts sobre la columna tomatometer_status
 
+df_movies_calificaciones= df_movies['tomatometer_status'].value_counts()
+print(df_movies_calificaciones)
+
+#10 •	Visualiza la distribución de calificaciones: Crea un gráfico circular (pie chart) simple para mostrar la proporción de cada tomatometer_status. Asegúrate de añadir etiquetas.
+
+#Propiedades del grafico
+plt.figure(figsize= (10,10))
+plt.pie(df_movies_calificaciones, labels=['Rotten', 'Fresh','Certified Fresh'], autopct='%1.0f%%', colors=['blue', 'orange', 'green'], startangle=140)
+plt.title("Distribución de calificaciones")
+plt.show()
