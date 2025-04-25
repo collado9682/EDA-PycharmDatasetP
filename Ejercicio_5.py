58f2ceea428ad78d3693939efbbfb961390127b1
collado9682 <genesiscollado813@gmail.com>
2025-04-24 23:31:54 -0600

    Finalizando el ejercicio, agrego el gráfico de barras

--- Ejercicio_5.py
+++ Ejercicio_5.py
@@ -22,6 +22,20 @@ print(top_10_directores)
 
 #Dataframe original
 top_10_nombres= top_10_directores.index.tolist()
-df_top_10= df_movies[df_movies['directors'].isin(top_10_nombres)].copy()
+df_top_10_directores= df_movies[df_movies['directors'].isin(top_10_nombres)].copy()
 
 #calcular el promedio de tomatometer_rating para peliculas de solo los 10 directores
+promedio_por_director= df_top_10_directores.groupby('directors')['tomatometer_rating'].mean().sort_values(ascending=False)
+print(promedio_por_director)
+
+# •	Crea un gráfico de barras simple que muestre el nombre de estos 5 directores y su tomatometer_rating promedio
+top_5= promedio_por_director.head(5)
+
+plt.figure(figsize = (8,8))
+plt.bar(top_5.index.tolist(), top_5.values, color = 'skyblue')
+plt.title("Calificación promedio de los top 5 directores más frecuentes")
+plt.xlabel('Director')
+plt.xticks(rotation=45, ha='right')
+plt.ylabel("Promedio Tomatómetro")
+plt.tight_layout()
+plt.show()
